f5cc4fe355b7f7bf7cf556639b6dc3ae960d698f
Gabriel B <gabelmo92@hotmail.com>
2020-01-25 17:02:19 +0100

    misc

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -20,7 +20,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped lang="scss">
 .home {
   display: flex;
   justify-content: center;
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -24,7 +24,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped lang="scss">
 .border-bottom {
   border-bottom: 2px solid black;
 }
